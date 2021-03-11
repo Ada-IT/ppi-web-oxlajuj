8d263f2f21cb1f8691eb2eaec505b9e4e78e147c
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-03-11 11:19:22 -0300

    se cambia class por className

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 const Navbar = () => {
   return (
-    <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-      <a class='navbar-brand' href='#'>
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <a className='navbar-brand' href='#'>
         Navbar
       </a>
       <button
-        class='navbar-toggler'
+        className='navbar-toggler'
         type='button'
         data-toggle='collapse'
         data-target='#navbarNav'
@@ -13,32 +13,27 @@ const Navbar = () => {
         aria-expanded='false'
         aria-label='Toggle navigation'
       >
-        <span class='navbar-toggler-icon'></span>
+        <span className='navbar-toggler-icon'></span>
       </button>
-      <div class='collapse navbar-collapse' id='navbarNav'>
-        <ul class='navbar-nav'>
-          <li class='nav-item active'>
-            <a class='nav-link' href='#'>
-              Home <span class='sr-only'>(current)</span>
+      <div className='collapse navbar-collapse' id='navbarNav'>
+        <ul className='navbar-nav'>
+          <li className='nav-item active'>
+            <a className='nav-link' href='#'>
+              Home <span className='sr-only'>(current)</span>
             </a>
           </li>
-          <li class='nav-item'>
-            <a class='nav-link' href='#'>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>
               Features
             </a>
           </li>
-          <li class='nav-item'>
-            <a class='nav-link' href='#'>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>
               Pricing
             </a>
           </li>
-          <li class='nav-item'>
-            <a
-              class='nav-link disabled'
-              href='#'
-              tabindex='-1'
-              aria-disabled='true'
-            >
+          <li className='nav-item'>
+            <a className='nav-link disabled' href='#' aria-disabled='true'>
               Disabled
             </a>
           </li>
