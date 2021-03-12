d7dfe956cb91bf0b15912f84acc0c0e05e83526c
Maria Hayward <hayward.maria@gmail.com>
2021-03-12 11:00:14 -0300

    subo carrousel con bug

--- components/carrusel/Carrusel.jsx
+++ components/carrusel/Carrusel.jsx
@@ -1,3 +1,56 @@
-const Carrusel = () => {};
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
 
-export default Carrusel;
+const CarouselComponent = () => {
+
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="./images/1.jpg?text=First slide&bg=373940"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="./images/2.jpg?text=First slide&bg=282c34"
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="./images/3.jpg?text=First slide&bg=20232a"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
+
+
+export default CarouselComponent;
--- pages/home/Home.jsx
+++ pages/home/Home.jsx
@@ -1,3 +1,5 @@
-const Home = () => {};
+import Carrusel from "../../components/carrusel/Carrusel";
 
+const Home = () => { };
+<Carrusel.jsx />
 export default Home;
