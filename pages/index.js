3ae7258d7af4694e2af480a69cba3381c1425974
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-03-12 12:27:09 -0300

    fix update

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
 import NavbarComponent from "../components/header/Navbar";
 import TopHeader from "../components/header/TopHeader";
 import Carrusel from "../components/carrusel/Carrusel";
-import Contacto from "./contacto";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       <TopHeader />
       <NavbarComponent />
-      <Navbar />
+
       <div className={styles.container}>
         <Carrusel></Carrusel>
       </div>
-      <Contacto />
     </>
   );
 }
