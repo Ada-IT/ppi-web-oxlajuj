ae314394677e102f54fa9f1fbfb0c4496ded6887
Mariana Castro <mb.castro17@gmail.com>
2021-03-12 11:26:25 -0300

    arreglo de carpetas

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/navbar/Navbar";
 import Carrusel from "../components/carrusel/Carrusel"
+
 export default function Home() {
   return (
     <>
