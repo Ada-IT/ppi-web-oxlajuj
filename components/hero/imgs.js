75dc876c77a81870a5c81b93c872aed312e88167
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-03-16 13:45:24 -0300

    se agrega archivo con imagenes del hero

---
+++ components/hero/imgs.js
@@ -0,0 +1,29 @@
+const imgs = [
+  {
+    type: "/quienes-somos",
+    img: "./images/news-bg.jpg",
+    title: "Quienes somos"
+  },
+  {
+    type: "/proyectos",
+    img: "./images/about-bg.jpg",
+    title: "Proyectos"
+  },
+  {
+    type: "/investigacion",
+    img: "./images/portfolio-bg.jpg",
+    title: "Investigación"
+  },
+  {
+    type: "/publicaciones",
+    img: "./images/news-bg.jpg",
+    title: "Publicacioness"
+  },
+  {
+    type: "/contacto",
+    img: "./images/contact-bg.jpg",
+    title: "Contacto"
+  }
+];
+
+export default imgs;
