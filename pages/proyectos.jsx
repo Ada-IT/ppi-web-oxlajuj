c22a0f28d0cb7dec82f7a8edd56b86c159652d07
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-03-16 13:46:56 -0300

    se agrega componente hero a todas las pages

--- pages/proyectos.jsx
+++ pages/proyectos.jsx
@@ -1,7 +1,12 @@
 import Header from "../containers/Header/header";
-
+import Hero from "../components/hero/Hero";
 const Projects = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 };
 
 export default Projects;
