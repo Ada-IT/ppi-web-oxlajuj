de6723736cddba04f0633065808fc5b5d85a66f9
Mariana Castro <mb.castro17@gmail.com>
2021-03-17 07:50:47 -0300

    arregloss

--- components/infoFundacion/InfoFundacion.jsx
+++ components/infoFundacion/InfoFundacion.jsx
@@ -1,19 +1,20 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
+//import Container from "react-bootstrap/Container";
 import { MapFill, Whatsapp, Mailbox, Envelope } from "react-bootstrap-icons";
 import style from "./InfoFundacion.module.css";
 
 const InfoFundacion = () => {
     return (
         <Row className={style.infoFundConten}>
+            
             <Col>
                 <Row className={style.infoFund}>
                     <Col xs="3" sm="3" md="3" lg="3">
                         <MapFill color='#48133b' size={35} />
 
                     </Col>
-                    <Col xs="9" sm="9" md="9" lg="9">
+                    <Col xs="3" sm="9" md="9" lg="9">
                         <p>Domicilio de la fundacion</p>
 
                     </Col>
@@ -22,7 +23,7 @@ const InfoFundacion = () => {
                     <Col xs="3" sm="3" md="3" lg="3">
                         <Envelope color='#48133b' size={35} />
                     </Col>
-                    <Col xs="9" sm="9" md="9" lg="9">
+                    <Col xs="3" sm="9" md="9" lg="9">
                         <p>Mail</p>
                     </Col>
                 </Row>
@@ -30,7 +31,7 @@ const InfoFundacion = () => {
                     <Col xs="3" sm="3" md="3" lg="3">
                         <Whatsapp color='#48133b' size={35} />
                     </Col>
-                    <Col xs="9" sm="9" md="9" lg="9">
+                    <Col xs="3" sm="9" md="9" lg="9">
                         <p>WhatsApp</p>
                     </Col>
                 </Row>
