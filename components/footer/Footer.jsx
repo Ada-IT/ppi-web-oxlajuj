4c9146d5b55ff57cb33ca6794f33faddfe8c258b
Mariana Castro <mb.castro17@gmail.com>
2021-03-18 21:35:57 -0300

    maquetado de footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -7,51 +7,29 @@ import Container from "react-bootstrap/Container";
 const Footer = () => {
     return (
         <Container className={style.backFooter} fluid>
-            <Row  >
-                <Col>
-                    <Row className="justify-content-md-center">
-                        <img style={{ width: "200px" }} src='.\images\LOGO_OXLAJU_BLANCO-1.png' />
-                    </Row>
-                    <Row className={style.infoFooterConten}>
-                        <Row className={style.infoFundConten}>
-                            <Col >
-                                <Col >
-                                    <Row className={style.infoFund}>
-                                        <Col xs="3" sm="3" md="3" lg="3">
-                                            <MapFill color='#FFFF' size={20} />
-
-                                        </Col>
-                                        <Col xs="9" sm="9" md="9" lg="9">
-                                            <p className={style.textStyle}>Domicilio de la fundacion</p>
-
-                                        </Col>
-                                    </Row>
-                                    <Row className={style.infoFund}>
-                                        <Col xs="3" sm="3" md="3" lg="3">
-                                            <Envelope color='#FFFF' size={20} />
-                                        </Col>
-                                        <Col xs="9" sm="9" md="9" lg="9">
-                                            <p className={style.textStyle}>Mail</p>
-                                        </Col>
-                                    </Row>
-                                    <Row className={style.infoFund}>
-                                        <Col xs="3" sm="3" md="3" lg="3">
-                                            <Whatsapp color='#FFFF' size={20} />
-                                        </Col>
-                                        <Col xs="9" sm="9" md="9" lg="9">
-                                            <p className={style.textStyle}>WhatsApp</p>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                            </Col>
-                        </Row>
-                    </Row>
+            <Row >
+                <Col  className={style.aca}>
+                    <div>
+                        <Col>
+                            <img style={{ width: "200px" }} src='.\images\LOGO_OXLAJU_BLANCO-1.png' />
+                            <Row>
+                                <MapFill color='#FFFF' size={15} />
+                                <p className={style.textStyle}>Domicilio de la fundacion</p>
+                            </Row>
+                            <Row>
+                                <Envelope color='#FFFF' size={15} />
+                                <p className={style.textStyle}>fundacionoxlajuj@gmail.com</p>
+                            </Row>
+                            <Row>
+                                <Whatsapp color='#FFFF' size={15} />
+                                <p className={style.textStyle}>41285119</p>
+                            </Row>
+                        </Col>
+                    </div>
                 </Col>
-                <Col>
+                <Col md="auto">
                     <Row className="justify-content-md-center">
-                        <Row>
-                            <h1 className={style.titleStyle}>Accesos rápidos </h1>
-                        </Row>
+                        <h1 className={style.titleStyle}>Accesos rápidos </h1>
                     </Row>
                     <Row className="justify-content-md-center">
                         <a href="https://www.google.com" className={style.access}>Google</a>
@@ -67,12 +45,18 @@ const Footer = () => {
                     <Row className="justify-content-md-center">
                         <h1 className={style.titleStyle}>Nuestras redes sociales</h1>
                     </Row>
-                    <Row className="justify-content-md-center">
-                        <a href="https://www.facebook.com/FundacionOxlajujnoj"><Facebook className={style.redes} /></a>
-                    </Row>
-                    <Row className="justify-content-md-center">
-                        <a href="https://www.instagram.com/fundacionoxlajujnoj/?hl=es-la"><Instagram className={style.redes} /></a>
-                    </Row>
+                    <div className={style.ContainerRedes}>
+                        <Row >
+                            <Col>
+                                <a href="https://www.facebook.com/FundacionOxlajujnoj">
+                                    <Facebook className={style.redes} />
+                                </a>
+                            </Col>
+                            <Col>
+                                <a href="https://www.instagram.com/fundacionoxlajujnoj/?hl=es-la"><Instagram className={style.redes} /></a>
+                            </Col>
+                        </Row>
+                    </div>
                 </Col>
             </Row>
         </Container>
