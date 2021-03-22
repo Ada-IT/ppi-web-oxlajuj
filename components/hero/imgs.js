4afda5b81731a8c4a924d307cc660663def3e53b
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-03-22 17:32:27 -0300

    se elimina comentarios del archivo

--- components/hero/imgs.js
+++ components/hero/imgs.js
@@ -18,14 +18,11 @@ const imgs = [
     type: "/publicaciones",
     img: "./images/news-bg.jpg",
     title: "Publicaciones"
-<<<<<<< HEAD
-=======
   },
   {
     type: "/equipo",
     img: "./images/portfolio-bg.jpg",
     title: "Equipo"
->>>>>>> 6ffd81066a099a32919a233c6045eb6e4f90e263
   },
   {
     type: "/contacto",
