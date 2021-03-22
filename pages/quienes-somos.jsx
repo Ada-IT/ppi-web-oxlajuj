c310ee8b00e7877f06cbd0833816bfb06c7be489
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-03-22 18:58:15 -0300

    se agrega estilos

--- pages/quienes-somos.jsx
+++ pages/quienes-somos.jsx
@@ -9,60 +9,73 @@ const WhoAre = () => {
 
       <Container>
         <div>
-          <h4 className={style.title}>
-            Fundación cuya naturaleza es carácter privado, no partidista, sin
-            fines de lucro, cuya proyección es social y de asistencia
-            humanitaria.
-          </h4>
+          <h2 className={style.title}>Nuestros Valores</h2>
         </div>
-
-        <div>
-          <h2>Nuestra Historia</h2>
-          <p>
-            En el año 2007 surgió la Fundación Oxlajuj No’j, en Guatemala. Un
-            proyecto en el que siete intelectuales e investigadoras de distintas
-            disciplinas se conformaron como grupo académico; con la firme
-            convicción que la academia y la investigación son fundamentales para
-            el desarrollo de la humanidad.
-          </p>
+        <div className='row'>
+          <div className='col-12  p-4 col-lg-5 offset-lg-1 text-center text-lg-left'>
+            <h3 class='value-title pt-1'>Nuestra Historia</h3>
+            <p>
+              En el año 2007 surgió la Fundación Oxlajuj No’j, en Guatemala. Un
+              proyecto en el que siete intelectuales e investigadoras de
+              distintas disciplinas se conformaron como grupo académico; con la
+              firme convicción que la academia y la investigación son
+              fundamentales para el desarrollo de la humanidad.
+            </p>
+            <img
+              className='pb-3'
+              aria-hidden='true'
+              src='./simbolo.png'
+              alt='nuestra-historia'
+            />
+          </div>
+          <div class='col-12  p-4 col-lg-5  text-center text-lg-left'>
+            <h3 class='value-title pt-1'>¿Quienes somos?</h3>
+            <p>
+              Somos una fundación cuya naturaleza es carácter privado, no
+              partidista, sin fines de lucro, con proyección social y de
+              asistencia humanitaria.
+            </p>
+            <img
+              class='pb-3'
+              aria-hidden='true'
+              src='./img3.png'
+              alt='mision'
+            />
+          </div>
         </div>
-        <div>
-          <h2>Visión</h2>
-          <p>
-            Educar a la población por medio de la formación e investigación en
-            temas de género, ética y derechos humanos, fomentando un
-            empoderamiento sostenible para las y los jóvenes y mujeres de la
-            región centroamericana. Aacortando las brechas de género y
-            disminuyendo la violencia basada en género. Con más de diez años de
-            experiencia en trabajo de campo en comunidades rurales con población
-            indígena, y con incidencia en once departamentos del país, además de
-            alianzas en países como Nicaragua, México y España.
-          </p>
-        </div>
-        <div>
-          <h2>Misión</h2>
-          <p>
-            Fortalecemos la ejecución de proyectos con enfoque de género y
-            empoderamiento sostenible para jóvenes y mujeres de áreas rurales,
-            por medio de nuestros programas educativos, investigaciones y
-            capacitaciones para profesionales.
-          </p>
-        </div>
-        <div>
-          <h2>Donde trabajamos</h2>
-          <ul>
-            <li>Guatemala</li>
-            <li>Jutiapa</li>
-            <li>Sololá</li>
-            <li>Quetzaltenango</li>
-            <li>Chimaltenango</li>
-            <li>Sacatepéquez</li>
-            <li>Alta Verapaz</li>
-            <li>San Marcos</li>
-            <li>Chiquimula</li>
-            <li>Escuintla</li>
-            <li>Totonicapán</li>
-          </ul>
+        <div className='row'>
+          <div className='col-12  p-4 col-lg-5 offset-lg-1 text-center text-lg-left'>
+            <h3 class='value-title pt-1'>Visión</h3>
+            <p>
+              Educar a la población por medio de la formación e investigación en
+              temas de género, ética y derechos humanos, fomentando un
+              empoderamiento sostenible para las y los jóvenes y mujeres de la
+              región centroamericana. Aacortando las brechas de género y
+              disminuyendo la violencia basada en género.
+            </p>
+            <img
+              class='pb-3'
+              aria-hidden='true'
+              src='./img1.png'
+              alt='vision'
+            />
+          </div>
+          <div class='col-12  p-4 col-lg-5 offset-lg-1 text-center text-lg-left'>
+            <h3 class='value-title pt-1'>Misión</h3>
+            <p>
+              Fortalecemos la ejecución de proyectos con enfoque de género y
+              empoderamiento sostenible para jóvenes y mujeres de áreas rurales,
+              por medio de nuestros programas educativos, investigaciones y
+              capacitaciones para profesionales.
+            </p>
+            <img
+              class='pb-3'
+              aria-hidden='true'
+              src='./img2.png'
+              alt='quienes-somos'
+              style={{ borderRadius: "50%", width: "196px", height: "212px" }}
+            />
+          </div>
         </div>
       </Container>
     </>
