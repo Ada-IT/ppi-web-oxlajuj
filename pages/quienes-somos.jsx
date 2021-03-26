59c420d7eed1d54e1bbd57ccffe7bb6613bd63c6
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-03-26 10:46:41 -0300

    se modifica imagenes

--- pages/quienes-somos.jsx
+++ pages/quienes-somos.jsx
@@ -6,77 +6,72 @@ const WhoAre = () => {
   return (
     <>
       <Hero />
-
       <Container>
-        <div>
-          <h2 className={style.title}>Nuestros Valores</h2>
-        </div>
-        <div className='row'>
-          <div className='col-12  p-4 col-lg-5 offset-lg-1 text-center text-lg-left'>
-            <h3 class='value-title pt-1'>Nuestra Historia</h3>
-            <p>
-              En el año 2007 surgió la Fundación Oxlajuj No’j, en Guatemala. Un
-              proyecto en el que siete intelectuales e investigadoras de
-              distintas disciplinas se conformaron como grupo académico; con la
-              firme convicción que la academia y la investigación son
-              fundamentales para el desarrollo de la humanidad.
-            </p>
-            <img
-              className='pb-3'
-              aria-hidden='true'
-              src='./simbolo.png'
-              alt='nuestra-historia'
-            />
+        <section className={style.section}>
+          <div className='row'>
+            <div className='col-lg-5 col-md-12'>
+              <h2 className={style.title}>Nuestra Historia</h2>
+              <p className={style.paragraph}>
+                En el año 2007 surgió la Fundación Oxlajuj No’j, en Guatemala.
+                Un proyecto en el que siete intelectuales e investigadoras de
+                distintas disciplinas se conformaron como grupo académico; con
+                la firme convicción que la academia y la investigación son
+                fundamentales para el desarrollo de la humanidad.
+              </p>
+            </div>
+            <div className='d-flex justify-content-center align-items-center offset-lg-1 col-lg-6 col-md-12'>
+              <img src='./images/history.jpeg' alt='nuestra-historia' />
+            </div>
           </div>
-          <div class='col-12  p-4 col-lg-5  text-center text-lg-left'>
-            <h3 class='value-title pt-1'>¿Quienes somos?</h3>
-            <p>
-              Somos una fundación cuya naturaleza es carácter privado, no
-              partidista, sin fines de lucro, con proyección social y de
-              asistencia humanitaria.
-            </p>
-            <img
-              class='pb-3'
-              aria-hidden='true'
-              src='./img3.png'
-              alt='mision'
-            />
+        </section>
+        <section className={style.section}>
+          <div className='row'>
+            <div className='d-flex justify-content-center align-items-center col-lg-6 col-md-12 '>
+              <img src='./images/mision.jpeg' alt='mision' />
+            </div>
+            <div class='offset-lg-1 col-lg-5 col-md-12'>
+              <h2 className={style.title}>¿Quienes somos?</h2>
+              <p>
+                Somos una fundación cuya naturaleza es carácter privado, no
+                partidista, sin fines de lucro, con proyección social y de
+                asistencia humanitaria.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className='row'>
-          <div className='col-12  p-4 col-lg-5 offset-lg-1 text-center text-lg-left'>
-            <h3 class='value-title pt-1'>Visión</h3>
-            <p>
-              Educar a la población por medio de la formación e investigación en
-              temas de género, ética y derechos humanos, fomentando un
-              empoderamiento sostenible para las y los jóvenes y mujeres de la
-              región centroamericana. Aacortando las brechas de género y
-              disminuyendo la violencia basada en género.
-            </p>
-            <img
-              class='pb-3'
-              aria-hidden='true'
-              src='./img1.png'
-              alt='vision'
-            />
+        </section>
+        <section className={style.section}>
+          <div className='row'>
+            <div className='col-lg-5 col-md-12 text-mision'>
+              <h2 className={style.title}>Visión</h2>
+              <p className={style.paragraph}>
+                Educar a la población por medio de la formación e investigación
+                en temas de género, ética y derechos humanos, fomentando un
+                empoderamiento sostenible para las y los jóvenes y mujeres de la
+                región centroamericana. Aacortando las brechas de género y
+                disminuyendo la violencia basada en género.
+              </p>
+            </div>
+            <div className='d-flex justify-content-center align-items-center offset-lg-1 col-lg-6 col-md-12 img-mision'>
+              <img class='pb-3' src='./images/vision.jpeg' alt='vision' />
+            </div>
           </div>
-          <div class='col-12  p-4 col-lg-5 offset-lg-1 text-center text-lg-left'>
-            <h3 class='value-title pt-1'>Misión</h3>
-            <p>
-              Fortalecemos la ejecución de proyectos con enfoque de género y
-              empoderamiento sostenible para jóvenes y mujeres de áreas rurales,
-              por medio de nuestros programas educativos, investigaciones y
-              capacitaciones para profesionales.
-            </p>
-            <img
-              class='pb-3'
-              aria-hidden='true'
-              src='./img2.png'
-              alt='quienes-somos'
-              style={{ borderRadius: "50%", width: "196px", height: "212px" }}
-            />
+        </section>
+        <section className={style.section}>
+          <div className='row'>
+            <div className='d-flex justify-content-center align-items-center  col-lg-6 col-md-12 img-mision'>
+              <img src='./images/quienes-somos.jpeg' alt='quienes-somos' />
+            </div>
+            <div class='offset-lg-1 col-lg-5 col-md-12 text-mision'>
+              <h2 className={style.title}>Misión</h2>
+              <p>
+                Fortalecemos la ejecución de proyectos con enfoque de género y
+                empoderamiento sostenible para jóvenes y mujeres de áreas
+                rurales, por medio de nuestros programas educativos,
+                investigaciones y capacitaciones para profesionales.
+              </p>
+            </div>
           </div>
-        </div>
+        </section>
       </Container>
     </>
   );
