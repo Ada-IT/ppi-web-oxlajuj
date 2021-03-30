2329b280b4b32a513a069517700401ecd0e1d024
Rocío Esteban <53129859+hrchioest@users.noreply.github.com>
2021-03-30 19:08:21 -0300

    Create dataAll.js
    update data

---
+++ components/listCardAll/dataAll.js
@@ -0,0 +1,216 @@
+const data = [
+    {
+        id: "101",
+        nombre: "Andrea Lopez",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Dirección de proyectos"
+    },
+
+    {
+        id: "102",
+        nombre: "Gabriela Gaytan",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Dirección de comunicaión"
+    },
+    {
+        id: "103",
+        nombre: "Alejandro Noj",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Dirección de investigación"
+    },
+    {
+        id: "104",
+        nombre: "Nahony Hernandez",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Dirección de alianzas y cooperación"
+    },
+    {
+        id: "105",
+        nombre: "Gabriela Pineda",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Dirección de estrategias y alianzas"
+    },
+    {
+        id: "106",
+        nombre: "Lily Araujo",
+        img:"./images/AlejandraTelegario.png",
+        cargo: "Dirección de equipo de investigación USAC"
+    },
+    {
+        id: "107",
+        nombre: "Diego taracena",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Dirección de equipo de investigación URL"
+    },
+    {
+        id: "108",
+        nombre: "Marce Guevara",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Dirección de investigación de dpto. de Escuintla"
+    },
+    {
+        id: "109",
+        nombre: "Karla Zúñiga",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Coordinadora de contenidos academicos"
+    },
+    {
+        id: "110",
+        nombre: "Iraydis Cardona",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Coordinadora de alianzas municipales"
+    },
+    {
+        id: "111",
+        nombre: "Jéssica Gómeza",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Cooperación y alianzas"
+    },
+    {
+        id: "112",
+        nombre: "Yamileth Ventura",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Cooperación y alianzas"
+    },
+    {
+        id: "113",
+        nombre: "Estefany Ruiz",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Cooperación y monitoreo"
+    },
+    {
+        id: "114",
+        nombre: "Gabriela López",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Cooperación y monitoreo"
+    },
+    {
+        id: "5",
+        nombre: "Carolina Rabe",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Coordinación de redes sociales"
+    },
+    {
+        id: "6",
+        nombre: "Jeimy Martinez",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Coordinación alianzas colegios"
+    },
+    {
+        id: "7",
+        nombre: "Alejandra Teleguario",       
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajadora en Quetaltenango"
+    },
+    {
+        id: "8",
+        nombre: "Wendy Orozco",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajadora en San Marcos"
+    },
+    {
+        id: "9",
+        nombre: "Isabel Bautista",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajadora en San Marcos"
+    },
+    {
+        id: "10",
+        nombre: "Cecilia Paiz",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajadora en San Marcos"
+    },
+    {
+        id: "11",
+        nombre: "Carlos ",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajador en Altaverapaz"
+    },
+    {
+        id: "12",
+        nombre: "Pablo",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajador en Guatemala"
+    },
+    {
+        id: "15",
+        nombre: "Ana Mux",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajadora en Guatemala"
+    },
+    {
+        id: "16",
+        nombre: "Elizabeth",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajadora en Sacatepéquez"
+    },
+    {
+        id: "17",
+        nombre: "Zayra",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajadora en Sacatepéquez"
+    },
+    {
+        id: "18",
+        nombre: "Elida",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajadora en Sololá"
+    },
+    {
+        id: "19",
+        nombre: "Ramón",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajador en Sololá"
+    },
+    {
+        id: "21",
+        nombre: "Ilse Cua Barreno",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Embajadora en Totonicapán"
+    },
+    {
+        id: "22",
+        nombre: "Iraydis Virgilia",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Pasantía de proyectos y cooperación"
+    },
+    {
+        id: "23",
+        nombre: "Zuli García",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Pasantía de proyectos y cooperación"
+    },
+    {
+        id: "24",
+        nombre: "Estefany Barraza",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Pasantía de proyectos y cooperación"
+    },{
+        id: "25",
+        nombre: "Andrea Vanegas",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Pasantía de proyectos y cooperación"
+    },{
+        id: "26",
+        nombre: "Vannesa Campos",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Pasantía de proyectos y cooperación"
+    },{
+        id: "27",
+        nombre: "Angela López",
+        img: "./images/AlejandraTelegario.png",
+        cargo: "Pasantía de proyectos y cooperación"
+    },{
+        id: "28",
+        nombre: "Alaide Iguardia",
+        img:"./images/AlejandraTelegario.png",
+        cargo: "Pasantía de proyectos y cooperación"
+    },
+    {
+        id: "29",
+        nombre: "Alejandra Teleguario",
+        img:"./images/AlejandraTelegario.png",
+        cargo: "Pasantía de proyectos y cooperación"
+    }
+];
+export default data;
