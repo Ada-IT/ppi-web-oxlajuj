9f34f66ea286018a576daed528f313cdbd1a3632
Rocío Esteban <53129859+hrchioest@users.noreply.github.com>
2021-03-30 19:02:03 -0300

    Create ListCardAll.jsx
    update component

---
+++ components/ListCardAll/ListCardAll.jsx
@@ -0,0 +1,22 @@
+import CardComponent from "../CardComponent/CardComponent";
+import data from "./dataAll";
+
+const ListCardAll = () => {
+  return (
+    <>
+      {data.map((card) => {
+        return (
+          <CardComponent
+            img={card.img}
+            key={card.id}
+            nombre={card.nombre}
+            cargo={card.cargo}
+            btn=''
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default ListCardAll;
