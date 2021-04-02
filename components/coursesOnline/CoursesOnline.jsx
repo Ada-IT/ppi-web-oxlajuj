8a49da937ee45e8ab98b616d3ededd9e6610f0e0
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-04-02 12:43:34 -0300

    Se agrega contenido de cursos y testimonios

--- components/coursesOnline/CoursesOnline.jsx
+++ components/coursesOnline/CoursesOnline.jsx
@@ -1,39 +1,38 @@
 import React from "react";
+import Testimonials from "../testimonials/Testimonials";
 import style from "./CoursesOnline.module.css";
+import courses from "./data";
 
 const CoursesOnline = () => {
   return (
     <div className={style.container}>
       <div className='container'>
+        <div className=''>
+          <h2 className={style.title}>Cursos online</h2>
+          <p className={style.paragraph}>
+            Los temas de los cursos que facilitamos son:
+          </p>
+          <div className='row'>
+            {courses.map((course) => (
+              <div className='col-6'>
+                <div className='p-3 m-2 border bg-light'>{course}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+
         <div className='row'>
-          <div className='col-lg-6 col-md-12 img-programa'>
+          {/* <div className='col-lg-5 col-md-12 img-programa'>
             <img
               src='images/collage.png'
               className={style.cursosImg}
               alt='cursos-online'
             />
-          </div>
-          <div className='offset-lg-1 col-lg-5 col-md-12 text-programa'>
-            <h2 className={style.title}>Cursos online</h2>
-            <p className={style.paragraph}>
-              Los temas de los cursos que facilitamos son: Inteligencia
-              emocional, Liderazgo colectivo, Prevención violencia sexual,Ciclo
-              violencia de género, Acciones positivas por la igualdad,
-              Elaboración de proyectos con perspectiva de género, Comunicación
-              con enfoque de género, Epistemologías feministas, Teoría feminista
-              para principiantes, Introducción a los ecofeminismos,
-              Empoderamiento feminista I, Empoderamiento feminista II, Derechos
-              de las mujeres y la Cedaw, Masculinidades disidentes, Mitos del
-              amor romántico, Construcción relaciones saludable, Enfoque de
-              género, Educación integral en sexualidad, Herramientas de
-              Investigación, Etica y bioetica, Resolución de conflictos,
-              Participación política de las mujeres, Democracias y juventudes
-              activas, Derechos de la comunidad LGTBIQ+, Salud con enfoque de
-              género, Estudios de la sexualidad, ODS y agenda 2030 y Feminismos
-              interseccionales.
-            </p>
-          </div>
+          </div> */}
         </div>
+      </div>
+      <div>
+        <Testimonials />
       </div>
     </div>
   );
