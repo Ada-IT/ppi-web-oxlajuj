3d1421ff8d73b99b4490e66174456f94ebc8429b
Maria Hayward <hayward.maria@gmail.com>
2021-04-07 16:39:27 -0300

    subo blog

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react';
 import Hero from "../components/hero/Hero";
+import { fetchBlogEntries } from './api/ApiContentful';
 
 const Blog = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  const getPost = async () => {
+    const entries = await fetchBlogEntries();
+    setPosts(entries);
+  }
+  useEffect(() => {
+    getPost();
+  }, [])
+
+
   return (
     <>
       <Hero />
+      <ul>
+        {posts.map(post => {
+          return (
+            <>
+              <li>titulo:{post.title}</li>
+              <img src={post.image.imageUrl} />
+            </>
+          )
+        })
+        }
+      </ul>
     </>
   );
 };
