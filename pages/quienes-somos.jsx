b71e36aec8b95eb662d932d5fc825a821404fcea
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-04-09 10:45:12 -0500

    se ajusta detales de estilos a quienes-somos

--- pages/quienes-somos.jsx
+++ pages/quienes-somos.jsx
@@ -6,72 +6,90 @@ const WhoAre = () => {
   return (
     <>
       <Hero />
-      <Container className={style.ContQuienes}>
+      <div class='container-fluid'>
         <Row className={style.row1}>
-          <Col sm={6} className={style.imgHistory} >
-            <img src='./images/quienesSomos/nuestraHistoria1.png' className={style.img} />
+          <Col sm={6} className={style.imgHistory}>
+            <img
+              src='./images/quienesSomos/nuestraHistoria1.png'
+              className={style.img}
+            />
           </Col>
           <Col sm={6}>
             <p className={style.title}>Nuestra Historia</p>
-            <p className={style.textQuienes}>En el año 2007 surgió la Fundación Oxlajuj No’j, en Guatemala.
-            Un proyecto en el que siete intelectuales e investigadoras de
-            distintas disciplinas se conformaron como grupo académico; con
-            la firme convicción que la academia y la investigación son
-            fundamentales para el desarrollo de la humanidad.</p>
+            <p className={style.textQuienes}>
+              En el año 2007 surgió la Fundación Oxlajuj No’j, en Guatemala. Un
+              proyecto en el que siete intelectuales e investigadoras de
+              distintas disciplinas se conformaron como grupo académico; con la
+              firme convicción que la academia y la investigación son
+              fundamentales para el desarrollo de la humanidad.
+            </p>
           </Col>
         </Row>
-        {/*         <hr noshade="noshade" className={style.linea} />
- */}        <Row className={style.row2}>
+        <Row className={style.row2}>
           <Col sm={6} className={style.rowQuienes2}>
-            <h1 className={style.title2} >¿Quienes somos?</h1>
-            <p className={style.textQuienes2} >Somos una fundación cuya naturaleza es carácter privado, no
-            partidista, sin fines de lucro, con proyección social y de
-                asistencia humanitaria.</p>
+            <h1 className={style.title2}>¿Quienes somos?</h1>
+            <p className={style.textQuienes2}>
+              Somos una fundación cuya naturaleza es carácter privado, no
+              partidista, sin fines de lucro, con proyección social y de
+              asistencia humanitaria.
+            </p>
           </Col>
-          <Col sm={6}  >
-            <img src='./images/quienesSomos/quienesSomos1.png' className={style.img} />
+          <Col sm={6}>
+            <img
+              src='./images/quienesSomos/quienesSomos1.png'
+              className={style.img}
+            />
           </Col>
         </Row>
-        {/*         <hr noshade="noshade" className={style.linea} />
- */}        <Row className={style.row1}>
-          <Col sm={6} className={style.imgHistory} >
-            <img src='./images/quienesSomos/vision1.png' className={style.img} />
+        <Row className={style.row1}>
+          <Col sm={6} className={style.imgHistory}>
+            <img
+              src='./images/quienesSomos/vision1.png'
+              className={style.img}
+            />
           </Col>
           <Col sm={6}>
             <p className={style.title}>Visión</p>
-            <p className={style.textQuienes}>Educar a la población por medio de la formación e investigación
-            en temas de género, ética y derechos humanos, fomentando un
-            empoderamiento sostenible para las y los jóvenes y mujeres de la
-            región centroamericana. Aacortando las brechas de género y
-                disminuyendo la violencia basada en género.</p>
+            <p className={style.textQuienes}>
+              Educar a la población por medio de la formación e investigación en
+              temas de género, ética y derechos humanos, fomentando un
+              empoderamiento sostenible para las y los jóvenes y mujeres de la
+              región centroamericana. Aacortando las brechas de género y
+              disminuyendo la violencia basada en género.
+            </p>
           </Col>
         </Row>
-        {/*         <hr noshade="noshade" className={style.linea} />
- */}        <Row className={style.row2}>
+        <Row className={style.row2}>
           <Col sm={6} className={style.rowQuienes2}>
             <p className={style.title2}>Misión</p>
-            <p className={style.textQuienes2} >Fortalecemos la ejecución de proyectos con enfoque de género y
-            empoderamiento sostenible para jóvenes y mujeres de áreas
-            rurales, por medio de nuestros programas educativos,
-                investigaciones y capacitaciones para profesionales.</p>
+            <p className={style.textQuienes2}>
+              Fortalecemos la ejecución de proyectos con enfoque de género y
+              empoderamiento sostenible para jóvenes y mujeres de áreas rurales,
+              por medio de nuestros programas educativos, investigaciones y
+              capacitaciones para profesionales.
+            </p>
           </Col>
           <Col sm={6}>
             <img src='./images/quienesSomos/mision.png' className={style.img} />
           </Col>
         </Row>
-        {/*         <hr noshade="noshade" className={style.linea} />
- */}        <Row className={style.row1}>
-          <Col sm={6} className={style.imgHistory} >
-            <img src='./images/quienesSomos/dondeTrabajamos1.png' className={style.img} />
+        <Row className={style.row1}>
+          <Col sm={6} className={style.imgHistory}>
+            <img
+              src='./images/quienesSomos/dondeTrabajamos1.png'
+              className={style.img}
+            />
           </Col>
-          <Col sm={6} >
+          <Col sm={6}>
             <p className={style.title}>¿Donde trabajamos?</p>
-            <p className={style.textQuienes}>Estamos en los siguientes departamentos: Guatemala, Jutiapa,
-            Sololá, Quetzaltenango, Chimaltenango, Sacatepéquez, Alta,
-                Verapaz, San Marcos, Chiquimula, Escuintla y Totonicapán.</p>
+            <p className={style.textQuienes}>
+              Estamos en los siguientes departamentos: Guatemala, Jutiapa,
+              Sololá, Quetzaltenango, Chimaltenango, Sacatepéquez, Alta,
+              Verapaz, San Marcos, Chiquimula, Escuintla y Totonicapán.
+            </p>
           </Col>
         </Row>
-      </Container>
+      </div>
     </>
   );
 };
