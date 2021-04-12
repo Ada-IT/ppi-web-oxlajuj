15668cb9f5f89a2d32f3a8d2951fcdf2dea9e24f
Rocio del Pilar Esteban <hr.chio@gmail.com>
2021-04-12 14:40:16 -0500

    se pasa datos al modal de blog

--- components/modalBlog/ModalBlog.jsx
+++ components/modalBlog/ModalBlog.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 
-const ModalBlog = ({ show, handleClose }) => {
-    return (
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
-          </Button>
-                <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-          </Button>
-            </Modal.Footer>
-        </Modal>
-    );
-}
+const ModalBlog = ({ show, handleClose, post }) => {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>{post.title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{post.publishedDate}</Modal.Body>
+      <img src={post.image.imageUrl} />
+      <Modal.Body>{post.description}</Modal.Body>
+      <Modal.Body>{post.body.content[0].content[0].value}</Modal.Body>
+      <Modal.Footer>
+        <Button variant='secondary' onClick={handleClose}>
+          Cerrar
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
 export default ModalBlog;
