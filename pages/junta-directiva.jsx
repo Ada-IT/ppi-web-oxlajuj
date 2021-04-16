1c5dc921ec9c361d69793c1d2e3013ac2559a3a2
Mariana Castro <mb.castro17@gmail.com>
2021-04-16 10:22:25 -0300

    correcciones de ortografia en jd

--- pages/junta-directiva.jsx
+++ pages/junta-directiva.jsx
@@ -13,7 +13,7 @@ const Junta = () => {
                     <Col className={style.jddesc}>
                         <Card.Title className={style.jdtitle}>Andrea Lourdes Lopez</Card.Title>
                         <Card.Text>
-                            Guatemalteca premiada por The Diana Award, UK , 2020 y la Organización Mundial Democrática como Mujer Líder y Emprendedora Social 2019, graduada a sus 22 años de la Universidad Rafael Landívar de la Licenciatura Investigación Criminal y Forense, y Licenciatura de Sociología de la Universidad San Carlos de Guatemala, con una trayectoria en investigación social, violencias, mujeres indígenas y juventudes. Fundadora de Dirección de Proyectos de Juventudes y Mujeres en la Fundación Oxlajuj N'oj. Ha dado ponencias presenciales en congresos internacionales: España, México,Costa Rica, Salvador y Perú. Se ha especializado en Investigación Social y Prevención de Violencia Sexual en Facultad Latinoamericana de Ciencias Sociales (FLACSO), activista en diferentes organizaciones como Fundacion Oxlajuj N'oj, Observatorio contra el Acoso Callejero y Jovénes Iberoamericanos. Miembro OSWD- La Organización de Mujeres en Ciencia para el Mundo en Desarrollo-  y Asociación para los Derechos de las Mujeres y el Desarrollo - AWID-. Actualmente cursa segundo año de máster en Estudios de Género y Mujeres- GEMMA ERASMUS MUNDUS- en Universidad de Bolonia Italia y Universidad de Granada España. Tiene publicaciones por la Universidad de Dresden Alemania, Universidad Salvador y otras revistas.
+                            Guatemalteca premiada por The Diana Award, UK, 2020 y la organización mundial democrática como mujer líder y emprendedora social 2019, graduada a sus 22 años de la Universidad Rafael Landívar de la licenciatura investigación criminal y forense, y licenciatura de sociología de la Universidad San Carlos de Guatemala, con una trayectoria en investigación social, violencias, mujeres indígenas y juventudes. Fundadora de Dirección de Proyectos de Juventudes y Mujeres en la Fundación Oxlajuj N'oj. Ha dado ponencias presenciales en congresos internacionales: España, México, Costa Rica, Salvador y Perú. Se ha especializado en investigación social y prevención de violencia sexual en facultad latinoamericana de ciencias sociales (FLACSO), activista en diferentes organizaciones como fundacion Oxlajuj N'oj, observatorio contra el acoso callejero y jovénes iberoamericanos. Miembro OSWD- La organización de mujeres en ciencia para el mundo en desarrollo -  y Asociación para los derechos de las mujeres y el desarrollo - AWID. Actualmente cursa segundo año de máster en Estudios de género y mujeres - GEMMA ERASMUS MUNDUS - en Universidad de Bolonia Italia y Universidad de Granada España. Tiene publicaciones por la Universidad de Dresden Alemania, Universidad Salvador y otras revistas.
                     </Card.Text>
                     </Col>
                 </Card.Body>
@@ -26,7 +26,7 @@ const Junta = () => {
                     <Col className={style.jddesc}>
                         <Card.Title className={style.jdtitle}>Luis López</Card.Title>
                         <Card.Text>
-                            Experto Internacional en Ética en Investigación con 30 años de experiencia en investigación en salud. Fue director de postgrado en ética en investigación proyecto auspiciado por el NIH de EE.UU programa internacional Fogarty en Guatemala. Es Doctor en Salud Pública del INSP de México; Máster en Bioética y Derecho de la Universidad de Barcelona España; Maestría en población por FLACSO- México; Médico y Cirujano USAC. Actualmente es Investigador en el CICS de la Facultad de Ciencias Médicas -USAC- y miembro del consejo asesor de la RedBioética /Unesco. A impartido docencia en temas de bioética y ética en la Universidad Rafael Landivar, Universidad Da Vinci, Universidad Galileo, Universidad de Panamá y Universidad de San Carlos de Guatemala. Miembro del Consejo Asesor RedBioética Unesco desde el año 2010.Tiene más de 40 publicación de artículos en revistas de arbitraje internacional y capítulos de libro, en temas de bioética, ética en investigación y salud pública.
+                            Experto internacional en ética, con 30 años de experiencia en investigación en salud. Fue director de postgrado en ética en investigación proyecto auspiciado por el NIH de EE.UU programa internacional Fogarty en Guatemala. Es Doctor en salud pública del INSP de México; máster en bioética y derecho de la Universidad de Barcelona España; Maestría en población por FLACSO- México; Médico y Cirujano USAC. Actualmente es investigador en el CICS de la facultad de ciencias médicas -USAC- y miembro del consejo asesor de la RedBioética /Unesco. A impartido docencia en temas de bioética y ética en la Universidad Rafael Landivar, Universidad Da Vinci, Universidad Galileo, Universidad de Panamá y Universidad de San Carlos de Guatemala. Miembro del consejo asesor RedBioética Unesco desde el año 2010.Tiene más de 40 publicación de artículos en revistas de arbitraje internacional y capítulos de libro, en temas de bioética, ética en investigación y salud pública.
                     </Card.Text>
                     </Col>
                 </Card.Body>
@@ -39,7 +39,7 @@ const Junta = () => {
                     <Col className={style.jddesc}>
                         <Card.Title className={style.jdtitle}>Marco Sagastume</Card.Title>
                         <Card.Text>
-                            Abogado y Notario USAC. Presidente del Comité de Derechos Humanos de la Federación Interamericana de Abogados -FIA-. Ex presidente del Foro Iberoamericano de Abogados. Ex presidente del Colegio de Abogados y Notarios de Guatemala.
+                            Abogado y Notario USAC. Presidente del comité de derechos humanos de la federación interamericana de abogados -FIA-. Ex presidente del foro iberoamericano de abogados. Ex presidente del colegio de abogados y notarios de Guatemala.
                     </Card.Text>
                     </Col>
                 </Card.Body>
@@ -52,8 +52,8 @@ const Junta = () => {
                     <Col className={style.jddesc}>
                         <Card.Title className={style.jdtitle}>Rodrigo Vargas</Card.Title>
                         <Card.Text>
-                            Químico Farmacéutico
-                            Co-fundador y director científico de Biofarn S.A . Actualmente es Investigador y consultor en ciencias farmacéuticas, en el área de biología molecular y química medicinal. Especialización Nanotecnología USAC-Guatemala 2009 Biología Molecular INVEGEM/USAC 2012 Máster Biotectnoloía Aliter-Madrid/España 2011 . Ha sido coordinador del área de Docencia en Química, Instituto de Investigaciones Químicas Biológicas Biomédicas y Biofísicas, Universidad Mariano Gálvez. Tienes más de 20 publicaciones en revistas indexadas
+                            Químico Farmacéutico,
+                            Co-fundador y director científico de Biofarn S.A . Actualmente es investigador y consultor en ciencias farmacéuticas, en el área de biología molecular y química medicinal. Especialización nanotecnología USAC-Guatemala 2009, Biología Molecular INVEGEM/USAC 2012 máster biotectnoloía Aliter-Madrid/España 2011 . Ha sido coordinador del área de docencia en química, Instituto de investigaciones químicas biológicas biomédicas y biofísicas, Universidad Mariano Gálvez. Tienes más de 20 publicaciones en revistas indexadas
 
                     </Card.Text>
                     </Col>
@@ -67,7 +67,7 @@ const Junta = () => {
                     <Col className={style.jddesc}>
                         <Card.Title className={style.jdtitle}>Marlen Judith Garnica Vanegas</Card.Title>
                         <Card.Text>
-                            Licenciada en Arqueología. Maestría en Historia, Escuela de Historia. Coordinadora de la Licenciatura en Arqueología, Escuela de Historia; USAC, segundo semestre de 2020 y primer semestre 2021. Apoyo al Proyecto de Revitalización del Museo de Historia Natural “Víctor Salvador de León Toledo” de la Casa de la Cultura de Occidente, Ciudad de Quetzaltenango, de 2013 a la fecha. Ha participado en congresos nacionales, simposios y diversos proyectos de investigación.
+                            Licenciada en arqueología. Maestría en historia, Escuela de historia. Coordinadora de la licenciatura en arqueología, Escuela de historia; USAC, segundo semestre de 2020 y primer semestre 2021. Apoyo al proyecto de revitalización del museo de historia natural “Víctor Salvador de León Toledo” de la Casa de la cultura de Occidente, Ciudad de Quetzaltenango, de 2013 a la fecha. Ha participado en congresos nacionales, simposios y diversos proyectos de investigación.
                     </Card.Text>
                     </Col>
                 </Card.Body>
@@ -80,7 +80,7 @@ const Junta = () => {
                     <Col className={style.jddesc}>
                         <Card.Title className={style.jdtitle}>Patricia Véliz</Card.Title>
                         <Card.Text>
-                            Licenciada en Psicología, Maestría en Medicina Social por la Universidad Autónoma de México, 1997. Doctora en Ciencias Sociales y Salud, Universidad de Barcelona, España, 2017. Actualmente profesor titular en la  Universidad de San Carlos de Guatemala.  Coordinadora de la Unidad de Apoyo y Desarrollo Estudiantil -UNADE-. Facultad de Ciencias Médicas, período 2019-2024. Vice-Presidenta Colegio de Psicólogos de Guatemala, Período 2021-2023.
+                            Licenciada en psicología, maestría en medicina social por la Universidad autónoma de México, 1997. Doctora en ciencias sociales y salud, Universidad de Barcelona, España, 2017. Actualmente profesor titular en la  Universidad de San Carlos de Guatemala.  Coordinadora de la Unidad de apoyo y desarrollo estudiantil -UNADE-. Facultad de ciencias médicas, período 2019-2024. Vice-Presidenta colegio de psicólogos de Guatemala, período 2021-2023.
                     </Card.Text>
                     </Col>
                 </Card.Body>
